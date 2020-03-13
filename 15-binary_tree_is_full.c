46b9a019967ac7a0fc033251bf5306ca45c9c50a
doubix <adib.grouz@gmail.com>
2020-03-13 21:33:41 +0100

    [add] is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * is_full_rec- Checks if a binary tree is full recursively.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 0 or 1.
+ */
+int is_full_rec(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_rec(tree->left) == 0 ||
+		    is_full_rec(tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: If tree is NULL or is not full - 0.
+ *         Otherwise - 1.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (is_full_rec(tree));
+}
