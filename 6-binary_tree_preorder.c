f413edba3ee8d2617f5ba777ab601c50b20cbc55
cedouiri <cedouiri@gmail.com>
2020-03-13 22:40:58 +0000

    add 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Goes through a binary tree using pre-order traversal.
+ * binary_tree_preorder - a function that goes through a
+ *binary tree using pre-order traversal.
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  * Return: void.
